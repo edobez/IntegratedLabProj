3e5b1ef3c44cb5ea00ae26538d1f2c51444379b6
Edoardo Bezzeccheri <coolman3@gmail.com>
2014-10-26 20:23:32 +0100

    Aggiunte FDT catena chiusa sistema complessivo

--- SystemModel.m
+++ SystemModel.m
@@ -4,10 +4,8 @@ s = tf('s');
 
 %% Parametri sistema
 Vdc = 30;
-% Kshunt = 1; % Guadagno current-sense
-Ktach = 1; % Guadagno speed-sense
-% Kc1_P = 3; % Guadagno controllore C1
-% Kc1_I = 4;
+Ktach = 6/(4000*2*pi/60); % Guadagno tach. [V/rad/s]
+
 C = 10e-9; % Capacità condensatore
 R1 = 1.8e3;
 R4 = 47e3;
@@ -21,7 +19,6 @@ Rv2.Minimum = 0;
 Rv2.Maximum = 10e3;
 Rv2_vals = [Rv2.Minimum:1e3:Rv2.Maximum];
 
-
 %% Parametri motore DC
 motor.rpm_max = 4000; % Max giri motore [rpm]
 motor.Tm_max = 0.06; % Coppia max [Nm]
@@ -34,7 +31,6 @@ motor.KE = motor.KT;
 motor.La = 2.8; % Induttanza di armatura [H]
 motor.Ra = 5.5; % Resistenza di armatura [Ohm]
 
-%% Dati ricavati dai parametri
 motor.omega_max = motor.rpm_max*2*pi/60; % Velocità angolare max [rad/s]
 motor.Ia_max = motor.Tm_max / motor.KT;
 
@@ -47,18 +43,17 @@ G2 = 1/(s*motor.J+motor.beta);
 G2.InputName = 'torque';
 G2.OutputName = 'rad/s';
 
-G1s = feedback(G1,motor.KE*motor.KT*G2);
+G3 = feedback(G1,motor.KE*motor.KT*G2);
 
 %% Calcolo fdt anello di corrente closed loop
 C1 = -R8/(s*R8*C+1);
-G = C1*-Vdc*G1; % Fdt in catena aperta con rotore bloccato
-Gs = C1*-Vdc*G1s; % Fdt in catena aperta con rotore libero
+G4blocked = C1*-Vdc*G1; % Fdt in catena aperta con rotore bloccato
+G4 = C1*-Vdc*G3; % Fdt in catena aperta con rotore libero
 
 H1 = -(R4*Rs/R1) * 1/(R7+Rv2);
-H2 = Ktach;
 
-W = 1/R6 * feedback(G,H1,+1);
-Ws = 1/R6 * feedback(Gs,H1,+1);
+W1blocked = 1/R6 * feedback(G4blocked,H1,+1);
+W1 = 1/R6 * feedback(G4,H1,+1);
 
 %% Calcolo fdt anello di velocità
 C2.Kp = 1;
@@ -68,8 +63,17 @@ C2.gamma = 0.1;
 C2.Tf = C2.gamma * C2.Kd / C2.Kp;
 C2 = pid(C2.Kp,C2.Ki,C2.Kd,C2.Tf);
 
+H2 = Ktach;
+
+G5 = C2 * G4 * motor.KT * G2; % Catena aperta 
+G5.InputName = 'voltage';
+G5.OutputName = 'omega';
+
+W2 = feedback(G5,H2)
+return;
+
 %% Step su W con Rv2 variabile
-Wsample = zpk(replaceBlock(Ws,'Rv2',Rv2_vals));
+Wsample = zpk(replaceBlock(W1,'Rv2',Rv2_vals));
 t = linspace(0,0.04,100);
 for i = 1:11
     y(:,i) = step(Wsample(:,:,1,i),t);
