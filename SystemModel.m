3c89bb193eb18bef1adfcacc3a6435b4c731945d
Edoardo Bezzeccheri <coolman3@gmail.com>
2014-10-27 22:38:49 +0100

    Lab. 27/10
    - Modificato PID
    - Aggiunta fdt tachimetro
    - Aggiunti modelli Simulink (ancora non completi)

--- SystemModel.m
+++ SystemModel.m
@@ -8,7 +8,7 @@ rpm2rad = (2*pi)/60;
 
 %% Parametri sistema
 Vdc = 30;
-Ktach = 6/(4000*rpm2rad); % Guadagno tach. [V/rad/s]
+Ktach = 8/(4000*rpm2rad); % Guadagno tach. [V/rad/s]
 
 C = 10e-9;      % Capacità condensatore
 R1 = 1.8e3;
@@ -16,9 +16,9 @@ R4 = 47e3;
 R6 = 100e3;
 R7 = 27e3;
 Rs = 0.1;       % Resistenza di shunt
-R8 = 350e3;     % DA MISURARE
+R8 = 10e6;     % DA MISURARE
 
-Rv2 = realp('Rv2', 10000);
+Rv2 = realp('Rv2', 0);
 Rv2.Minimum = 0;
 Rv2.Maximum = 10e3;
 
@@ -27,7 +27,7 @@ motor.rpm_max = 4000;       % Max giri motore [rpm]
 motor.Tm_max = 0.06;        % Coppia max [Nm]
 
 motor.J = 5.18e-6;          % Inerzia del motore []
-motor.beta = 5e-6;          % Attrito viscoso motore [Nm*s/rad] - DA MISURARE
+motor.beta = 0;          % Attrito viscoso motore [Nm*s/rad] - DA MISURARE
 motor.KT = 0.046;           % Costante di coppia [Nm/A]
 motor.KE = motor.KT;
 
@@ -57,16 +57,53 @@ H1 = -(R4*Rs/R1) * 1/(R7+Rv2);
 
 W1blocked = 1/R6 * feedback(G4blocked,H1,+1);       % fdt rotore bloccato
 W1 = 1/R6 * feedback(G4,H1,+1);                     % fdt closed-loop anello corrente
+% figure, step(W1);
 
 %% Calcolo fdt anello di velocità
-pid.Kp = realp('pidKp',1.66);           % costante proporzionale PID
-pid.Ki = realp('pidKi',3.25);           % costante integrativa PID
-pid.Kd = realp('pidKd',-0.0236);        % costante derivativa PID
+% pid.Kp = realp('pidKp',1.66);           % costante proporzionale PID
+% pid.Ki = realp('pidKi',3.25);           % costante integrativa PID
+% pid.Kd = realp('pidKd',-0.0236);        % costante derivativa PID
 % C2.gamma = 0.1;                       
-pid.Tf = realp('pidTf',0.05);           % coefficiente di filtro
-C2 = pid.Kp + pid.Ki/s + pid.Kd*s/(pid.Tf*s+1);
+% pid.Tf = realp('pidTf',0.05);           % coefficiente di filtro
 
-H2 = Ktach;
+pid.R5 = 10e3;
+pid.R8 = 10e3;
+pid.P2 = realp('P2',5e3); % Potenziometro P2
+pid.P2.Minimum = 0;
+pid.P2.Maximum = 1e6;
+pid.C3 = 1e-9;
+pid.p = -1/pid.R5 * (pid.R8 + pid.P2) / (s*(pid.R8+pid.P2)*pid.C3 + 1);
+pid.Kp = (pid.R8 + pid.P2)/pid.R5;
+
+pid.R6 = 4.7e3;
+pid.R9 = 1e6;
+pid.P1 = realp('P1',100e3);
+pid.P1.Minimum = 0;
+pid.P1.Maximum = 100e3;
+pid.C4 = 1e-6;
+pid.i = -1/(pid.P1+pid.R6) * (pid.R9)/(s*pid.R9*pid.C4+1);
+pid.Ki = 1/(pid.C4*(pid.P1+pid.R6));
+
+pid.R10 = 1e3;
+pid.P3 = realp('P3',1e3);
+pid.P3.Minimum = 0;
+pid.P3.Maximum = 1e6;
+pid.C1 = 4.7e-9;
+pid.C2 = 1e-6;
+pid.d = -(s*(pid.R10+pid.P3)*pid.C2)/(1+s*(pid.R10+pid.P3)*pid.C1);
+pid.Kd = pid.C2*(pid.P3+pid.R10);
+double([pid.Kp pid.Ki pid.Kd])
+
+C2 = -(pid.p + pid.i + pid.d);
+
+tach.C1 = 1e-6;
+tach.R1 = 10e3;
+tach.Rv1 = 47e3;
+tach.Rv1_2 = 40.8e3; % Parte del potenziometro fra il pin centrale e massa
+tach.Rv1_1 = tach.Rv1 - tach.Rv1_2;
+
+K = 0.0267; % Permette di avere un rapporto 8V/4000rpm
+H2 = K * tach.Rv1_2 / ( s*tach.Rv1*tach.R1*tach.C1 + tach.Rv1 + tach.R1);
 
 G5 = W1 * motor.KT * G2;    % Catena aperta plant
 G5.InputName = 'voltage';
@@ -74,19 +111,18 @@ G5.OutputName = 'omega';
 
 G5_uf = minreal(zpk(G5))*H2;
 
-W2 = feedback(C2*G5,H2)*rad2rpm;    % Catena chiusa totale - uscita in rpm
-step(W2)
+W2 = feedback(C2*G5,H2);    % Catena chiusa totale
+figure, step(W2*rad2rpm)
 
 %% Step su W con parametri variabili
-Rv2_vals = linspace(Rv2.Minimum,Rv2.Maximum,4);
-pidKp_vals = linspace(1,2,4);
-[Rv2grid,pidKpgrid] = ndgrid(Rv2_vals,pidKp_vals);
-Wsample = replaceBlock(W2,'Rv2',Rv2_vals);
-Wsample = replaceBlock(Wsample,'pidKp',pidKp_vals');
-Wsample.SamplingGrid = struct('Rv2',Rv2grid,'pidKp',pidKpgrid);
+% Rv2_vals = linspace(Rv2.Minimum,Rv2.Maximum,11);
+% pidKp_vals = linspace(1,2,4);
+% [Rv2grid,pidKpgrid] = ndgrid(Rv2_vals,pidKp_vals);
+% Wsample = replaceBlock(W1blocked,'Rv2',Rv2_vals);
+% Wsample = replaceBlock(Wsample,'pidKp',pidKp_vals');
+% Wsample.SamplingGrid = struct('Rv2',Rv2grid,'pidKp',pidKpgrid);
 
-% 
-% Wsample = zpk(replaceBlock(W1,'Rv2',Rv2_vals));
+% Wsample = zpk(replaceBlock(W1blocked,'Rv2',Rv2_vals));
 % t = linspace(0,0.04,100);
 % for i = 1:11
 %     y(:,i) = step(Wsample(:,:,1,i),t);
