0a30c7eca84114982109c919130f3a15548d5248
Edoardo Bezzeccheri <coolman3@gmail.com>
2014-11-23 21:24:24 +0100

    Fix funzione calcolo beta

--- betacalcs.m
+++ betacalcs.m
@@ -12,24 +12,35 @@ beta = (tau - co) ./ sp;
 %% Plots data
 close all;
 
-figure;
+f1 = figure;
 hold on;
 
 yfit = polyval(p,[sp(1) sp(end)]);
 plot(sp,tau);
 plot([sp(1) sp(end)],yfit,'--');
-title('Shaft speed vs Friction torque');
+title('Friction torque');
 xlabel('RPM');
 ylabel('Nm');
-text(500,0.015,sprintf('Fitted Beta = %.2g Nm/RPM',betafit));
-text(500,0.0145,sprintf('Coulomb friction = %.2g Nm',co));
+text(500,0.015,sprintf('Linearized Beta = %.2g Nm/RPM',betafit));
+text(500,0.0145,sprintf('Linearized Coulomb friction = %.2g Nm',co));
 hold off;
 
 figure;
 hold on;
 
 plot(sp,beta);
-title('Shaft speed vs Damping factor');
+title('Beta (RPM)');
 xlabel('RPM');
+ylabel('Nm/RPM');
+plot([0 4000],[betafit betafit],'--')
+text(1000,1e-6,sprintf('Linearized Beta = %.2g Nm/RPM',betafit));
+
+figure;
+hold on;
+
+plot(sp*rpm2rad,beta/rpm2rad);
+title('Beta (rad/s)');
+xlabel('rad/s');
 ylabel('Nm*s/rad');
-plot([0 4000],[betafit betafit])
+plot([0 450],[betafit/rpm2rad betafit/rpm2rad],'--');
+text(100,0.75e-5,sprintf('Linearized Beta = %.3g Nm/rad/s',betafit/rpm2rad));
